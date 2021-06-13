d8075ae721ec5c07e1150fd55d22694aca559d18
6a67 <83820531+6a67@users.noreply.github.com>
2021-06-13 20:25:39 +0200

    added javadoc

--- blatt08/src/DecoReader.java
+++ blatt08/src/DecoReader.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 public class DecoReader extends BufferedReader {
 
     private int lineNumber = 0;
-    private String expr = null;
+    private final String expr;
     private String lastLine = null;
 
     public DecoReader(Reader in, String expr) {
@@ -15,17 +15,30 @@ public class DecoReader extends BufferedReader {
         this.expr = expr;
     }
 
+    /**
+     * Returns the next line
+     * @return next line or null if there is no next line
+     * @throws IOException
+     */
     @Override
     public String readLine() throws IOException {
-        lineNumber++;
         lastLine = super.readLine();
+        if(lastLine != null) lineNumber++;
         return lastLine;
     }
 
+    /**
+     * Return the line number of the last line returned by readLine()
+     * @return the line number
+     */
     public int getLineNumber() {
         return lineNumber;
     }
 
+    /**
+     * Matches the last read line with the regex pattern and returns how often per line it is matched
+     * @return matches per line
+     */
     public int getAmountOfMatches() {
         if(expr == null) return 0;
         if(lastLine == null) return 0;
